584c07236a7924cf9ac0271e72e5e3191f82155d
javut <jaider.vergara@globalbank.com.pa>
2022-05-08 20:40:11 -0500

    Add useMemo

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,25 +1,64 @@
-import React from 'react'
-import { Navigate, useParams } from 'react-router-dom'
+import React, { useMemo, useState } from 'react'
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import { getHeroById } from '../../selectors/getHeroById';
 
 export const Hero = () => {
 
-  const {heroeId} = useParams();
+  const { heroeId } = useParams();
 
-  const hero = getHeroById(heroeId);
+  const hero = useMemo(() => getHeroById(heroeId), [heroeId]);
 
-  if(!hero){
-    return <Navigate to='/'/>
+  const navigate = useNavigate();
+
+  const handleReturn = () => {
+    navigate(-1); //Para regresar a pantalla anterior...
   }
 
 
-  return (
-    <div>
-        <h1>SOY UN HEROE MUY IMPORTANTE !!</h1>
-        <p>
-          {hero.superhero}
-        </p>
+  if (!hero) {
+    return <Navigate to="/" />
+  }
 
+  const {
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters
+  } = hero;
+
+  const imagePath = `/assets/${id}.jpg`;
+
+
+  return (
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={imagePath}
+          alt={superhero}
+          className="img-thumbnail"
+        ></img>
+      </div>
+
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego: </b>{alter_ego}</li>
+          <li className="list-group-item"><b>Publisher: </b>{publisher}</li>
+          <li className="list-group-item"><b>First Appearance: </b>{first_appearance}</li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{characters}</p>
+
+        <button
+          className="btn btn-outline-info"
+          onClick={handleReturn}
+        >
+          Regresar
+        </button>
+
+      </div>
 
     </div>
   )
